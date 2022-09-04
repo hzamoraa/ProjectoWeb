170cf2a9e8e50c2007205355347b2f5cd9e299ca
hzamora <hiranzamoraarcila@gmail.com>
2022-09-03 22:43:03 -0300

    Rutas y secciones

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page (index.ejs). */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET Sección (nosotros.ejs). */
+router.get('/nosotros', function(req, res, next) {
+  res.render('nosotros', { title: 'Nosotros somos....' });
+});
+/* GET Sección (productos.ejs). */
+router.get('/productos', function(req, res, next) {
+  res.render('productos', { title: 'Nuestros productos....' });
+});
+
 
 module.exports = router;
